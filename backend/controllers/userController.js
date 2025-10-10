908cdd850dc6c524a26f9afbb237c67d8754e72a
Thai Bao <bao223447@student.nctu.edu.vn>
2025-10-10 21:19:17 +0700

    Cập nhật backend - thêm model và sửa controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,34 @@
-let users = [];
+// let users = [];
 
-exports.getUsers = (req, res) => res.json(users);
+// exports.getUsers = (req, res) => res.json(users);
 
-exports.createUser = (req, res) => {
-  const newUser = { id: Date.now(), ...req.body };
-  users.push(newUser);
-  res.status(201).json(newUser);
+// exports.createUser = (req, res) => {
+//   const newUser = { id: Date.now(), ...req.body };
+//   users.push(newUser);
+//   res.status(201).json(newUser);
+// };
+
+
+// controllers/userController.js
+const User = require('../models/User');
+
+// GET /users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find(); // lấy dữ liệu thật
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// POST /users
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save(); // lưu vào MongoDB
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
