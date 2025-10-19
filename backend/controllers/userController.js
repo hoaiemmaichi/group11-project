c6ff9f0b2d8235789cd2066dbc6796c672047ea7
hoaiem <em222161@student.nctu.edu.vn>
2025-10-19 19:59:35 +0700

    Thêm API /signup, /login, /logout: kiểm tra email trùng, mã hóa mật khẩu bằng bcrypt, xác thực JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,42 +1,3 @@
-
-// let users = [];
-
-// exports.getUsers = (req, res) => res.json(users);
-
-// exports.createUser = (req, res) => {
-//   const newUser = { id: Date.now(), ...req.body };
-//   users.push(newUser);
-//   res.status(201).json(newUser);
-// };
-
-
-// controllers/userController.js
-const User = require('../models/User')
-
-// Lấy danh sách user
-exports.getUsers = async (req, res) => {
-  try {
-    const users = await User.find()
-    res.json(users)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-}
-
-// Thêm user mới
-exports.createUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body
-    const newUser = new User({ name, email, password })
-    await newUser.save()
-    res.status(201).json({ message: 'Tạo user thành công!', user: newUser })
-  } catch (error) {
-    res.status(400).json({ message: error.message })
-  }
-}
-
-
-
 const mongoose = require('mongoose');
 const User = require('../models/User');
 
@@ -46,7 +7,6 @@ let memoryUsers = [];
 // GET: lấy danh sách user (ưu tiên từ MongoDB)
 exports.getUsers = async (req, res) => {
   try {
-
     // explicitly exclude password field
     const users = await User.find().select('-password');
     return res.json(users);
@@ -149,5 +109,4 @@ exports.updateProfile = async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 };
-
 
